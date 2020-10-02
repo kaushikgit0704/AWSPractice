648ef801b450b7653916a53dccd153dba4ddcdb9
kaushikgit0704 <51095770+kaushikgit0704@users.noreply.github.com>
2020-10-02 23:12:58 +0530

    initial commit for ec2 deployment

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dot_net_core_ec2_deploy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
